a3552db8133deff50474b069a632381488430883
JVJ2001 <64433437+jayavishvaa@users.noreply.github.com>
2021-08-12 17:54:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View,Text,TouchableOpacity} from 'react-native';
-import Header from './Constants/Header';
-import Screen from './Constants/Screen';
 import MapNavigation from './Components/Screens/MapView';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
 import routes from './Components/routes';
 import ChooseLocation from './Components/Screens/ChooseLocation';
-import FlashMessage from "react-native-flash-message";
+import FlashMessage from "react-native-flash-message";  // Tried to create flash message using this package but not that effective remove if u don't want
+
+// Initiazed Stack navigation using React navigation version 5
 
 const Stack = createStackNavigator();
 
